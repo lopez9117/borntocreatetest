29806cd0d80565721d6a960408bca24b2a315d8d
luis.lopez <luis.lopez@contactica-i.com>
2019-02-03 07:13:17 -0500

    delete Pagination

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,13 +35,6 @@
 
 
 
-    <div class="pagination">
-        <ul class="list-unstyled container-flex space-center">
-            <li><a href="#" class="pagination-active">1</a></li>
-            <li><a href="#">2</a></li>
-            <li><a href="#">3</a></li>
-        </ul>
-    </div>
 
        <section class="footer">
         <footer>
