2e040a1abbbf9dc8b599cb29b6da4c0c02be5926
luis.lopez <luis.lopez@contactica-i.com>
2019-02-03 23:21:40 -0500

    adding a view to search columns

---
+++ database/migrations/2019_02_04_041334_postsandauthors.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Postsandauthors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+                   DB::statement("
+            CREATE VIEW postsandauthors 
+            AS
+            SELECT
+                posts.id AS id,
+                posts.title As title,
+                authors.firstName AS firstName,
+                authors.lastName AS lastName,
+                posts.content AS content,
+                posts.published_at AS published_at,
+              
+            FROM
+                posts
+                INNER JOIN authors ON posts.author_id = authors.id
+             
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
